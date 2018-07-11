21ae0bb7ed08faa1780a59eee181684b9b08bb27
Michael Minella <mminella@pivotal.io>
2018-07-11 14:01:29 -0500

    Initial commit

---
+++ src/main/java/io/spring/batch/bootbatchdemo/BootBatchDemoApplication.java
@@ -0,0 +1,12 @@
+package io.spring.batch.bootbatchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootBatchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootBatchDemoApplication.class, args);
+	}
+}
